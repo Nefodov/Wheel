2137b1e7064b42ebff3eab03212febc960c56c9d
OleksiiNefodov <siam9g@gmail.com>
2019-05-01 23:49:19 +0300

    some bases

---
+++ Assets/Scripts/UISystem/UIPopup.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class UIPopup : MonoBehaviour
+{
+
+	public void Open()
+	{
+
+	}
+
+	public void Close()
+	{
+
+	}
+}
---
+++ Assets/Scripts/UISystem/UIPopupReference.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class UIPopupReferenceBase : ScriptableObject
+{
+	private UIPopup popup;
+
+	public UIPopup Popup
+	{
+		get
+		{
+			if (popup == null)
+			{
+
+			}
+			return popup;
+		}
+	}
+
+	public abstract UIPopup GetPopup();
+	public abstract void Open();
+	public abstract void OpenInQueue();
+	public abstract void Close();
+}
