cebbab2023f2293dba2db4c04dcfa140b287278a
Oleksii <oleksii.nefodov@gmail.com>
2026-02-27 17:31:18 +0200

    new InputSystem

---
+++ Assets/Scripts/InputToFloatAction.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using ValueObjects;
+
+public class InputToFloatAction : MonoBehaviour
+{
+    public InputActionReference inputAction;
+    public ActionFloatObject eventObject;
+    public float k;
+
+    private Coroutine _holdingCoroutine;
+
+    private void OnEnable()
+    {
+        if (inputAction?.action != null)
+        {
+            inputAction.action.Enable();
+            inputAction.action.started += StartHolding;
+            inputAction.action.canceled += StopHolding;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (inputAction?.action != null)
+        {
+            StopHolding(default);
+            inputAction.action.started -= StartHolding;
+            inputAction.action.canceled -= StopHolding;
+            inputAction.action.Disable();
+        }
+    }
+
+    private void StartHolding(InputAction.CallbackContext context)
+    {
+        if (_holdingCoroutine == null)
+        {
+            _holdingCoroutine = StartCoroutine(HoldRoutine());
+        }
+    }
+
+    private void StopHolding(InputAction.CallbackContext context)
+    {
+        if (_holdingCoroutine != null)
+        {
+            StopCoroutine(_holdingCoroutine);
+            _holdingCoroutine = null;
+
+            eventObject.Invoke(0);
+        }
+    }
+
+    private IEnumerator HoldRoutine()
+    {
+        while (true)
+        {
+            float value = inputAction.action.ReadValue<float>();
+            eventObject.Invoke(value * k);
+
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
